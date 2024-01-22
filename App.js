aeccbe12dd8a11890a280646ef18183b1aad51bd
Nguyễn Phúc Hậu <nguyenphuchau@MacBook-Pro-cua-Nguyen.local>
2024-01-22 17:03:01 +0700

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from"react";
+import {SafeAreaView} from 'react-native';
+import Splash from "./src/screens//auth/Splash";
+import SignUp from "./src/screens//auth/SignUp";
+import SignIn from "./src/screens//auth/SignIn";
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+const Stack = createNativeStackNavigator();
+const App = () => {
 
-export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+    <NavigationContainer>
+      <Stack.navigator>
+        <Stack.Screen name="Splash" component={Splash} />
+        <Stack.Screen name="SignIn" component={SignIn} options={{headerShown:false}} />
+        <Stack.Screen name="SignUp" component={SignUp} options={{headerShown:false}} />
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+      </Stack.navigator>
+    </NavigationContainer>
+  )
+};
+
+export default App;
+
+
